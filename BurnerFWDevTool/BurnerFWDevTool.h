289a113ff9fc3f3ce5d9196ca335a7eb01dcf069
D2B1.ht_cheng <ht_cheng@phison.com>
2020-07-14 10:18:23 +0800

    Add project files.

---
+++ BurnerFWDevTool/BurnerFWDevTool.h
@@ -0,0 +1,32 @@
+
+// BurnerFWDevTool.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CBurnerFWDevToolApp:
+// See BurnerFWDevTool.cpp for the implementation of this class
+//
+
+class CBurnerFWDevToolApp : public CWinApp
+{
+public:
+	CBurnerFWDevToolApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBurnerFWDevToolApp theApp;
